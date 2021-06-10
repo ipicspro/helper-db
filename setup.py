b4b51289daae07d3c872ebd0e169aeceaad7fb12
ipicspro <36360306+ipicspro@users.noreply.github.com>
2021-06-10 17:55:12 +0300

    depend

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@ from setuptools import setup, find_packages
 
 
 setup(name='helper-db',
-      version='0.1',
-      url='https://github.com/ipicspro/helper-db',
-      license='MIT',
-      author='IS',
-      author_email='info@ecommaker.com',
-      description='helper-db',
-      #packages=find_packages(exclude=['tests']),
-      packages=['helper_db'],
-      long_description=open('README.md').read(),
-      zip_safe=False,
-      setup_requires=['nose', 'mysqlclient'],
-      test_suite='nose.collector')
+    version='0.1',
+    url='https://github.com/ipicspro/helper-db',
+    license='MIT',
+    author='IS',
+    author_email='info@ecommaker.com',
+    description='helper-db',
+    #packages=find_packages(exclude=['tests']),
+    packages=['helper_db'],
+    long_description=open('README.md').read(),
+    zip_safe=False,
+    setup_requires=['nose'],
+    install_requires=['mysqlclient'],
+    test_suite='nose.collector')
 
